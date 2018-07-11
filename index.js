2050c729b17543249e15457cbe26c884019b356f
Marnie Reid <marniejeanreid@gmail.com>
2018-07-11 13:06:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}` ;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)!}` ;
