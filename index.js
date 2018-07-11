e3008f0816a515257d09fc06a26b6f09ba87d529
Marnie Reid <marniejeanreid@gmail.com>
2018-07-11 12:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = 'Marnie Reid';
-const welcomeMessage = 'Welcome to Klossybook, ' currentUser;
+// const welcomeMessage = 'Welcome to Klossybook, ' currentUser;
